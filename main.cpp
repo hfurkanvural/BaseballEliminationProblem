dd278e0061409ce907070b7b166aa54e8864713e
hfurkanvural <hfurkanvural@gmail.com>
2020-03-23 15:29:45 +0300

    files updated

--- main.cpp
+++ main.cpp
@@ -15,29 +15,10 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include "Team.h"
 
 using namespace std;
 
-class Team{
-    string name;
-    int score;
-    int matchleft;
-    bool canWin;
-    float avr;
-public:
-    Team(){};
-    void setname(string x){name = x;}
-    void setscore(string x){score = stoi(x);}
-    void setmatchleft(int x){matchleft = x;}
-    void setcanWin(bool x) {canWin = x;}
-    void setavr(float x){avr = x;}
-    string outname(){return name;}
-    int outscore(){return score;}
-    float outavr(){return avr;}
-    int outmatchleft(){return matchleft;}
-    bool outcanWin(){return canWin;}
-
-};
 
 void bpm(Team *teams, int N)
 {
